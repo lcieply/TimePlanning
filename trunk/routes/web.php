5dc6d02519ae988f46a70467c883d34bac9e7d79
Łukasz Ciepły <lukaszcieply94@gmail.com>
2016-12-04 14:12:39 +0100

    PlansView + MettingsInfo

--- trunk/routes/web.php
+++ trunk/routes/web.php
@@ -20,7 +20,10 @@ Auth::routes();
 Route::get('user/activation/{token}', 'Auth\LoginController@activateUser')->name('user.activate');
 
 Route::get('/home', 'HomeController@index');
-
-
+Route::get('/mettingsInfo', 'MettingsInfoController@index');
+Route::get('/plansView', 'PlansViewController@index');
+//Na wypadek gdyby ktos uzywal samych malych liter
+Route::get('/mettingsinfo', 'MettingsInfoController@index');
+Route::get('/plansview', 'PlansViewController@index');
 
 
