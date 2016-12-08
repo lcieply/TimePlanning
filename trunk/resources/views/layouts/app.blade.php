8e3460f9ce4bb6e63df9ada91c0c41373bc53b96
student <marta.manka.95@gmail.com>
2016-12-09 00:02:31 +0100

    TASK 29 - Planowany wygląd strony - zmienienie kolorystyki, podświetlenie po najeździe kursorem, Menu i stopka  - osadzone, efekt na stronie głównej

--- trunk/resources/views/layouts/app.blade.php
+++ trunk/resources/views/layouts/app.blade.php
@@ -12,84 +12,103 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     <!-- Scripts -->
     <script>
+
         window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
+                'csrfToken' => csrf_token(),
         ]); ?>
     </script>
 
     <script src="/js/app.js"></script>
 
     @yield('script')
-
 </head>
+
+
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} {{ Auth::user()->surname }}<span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/manageAccount') }}">
-                                            Manage Account
-                                        </a>
-
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
             </div>
-        </nav>
 
-        @yield('content')
-    </div>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">ZALOGUJ</a></li>
+                        <li><a href="{{ url('/register') }}">REJESTRACJA</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} {{ Auth::user()->surname }}<span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ url('/manageAccount') }}">
+                                        USTAWIENIA KONTA
+                                    </a>
+
+
+                                </li>
+                                <li>
+                                    <a href="{{ url('/logout') }}"
+                                       onclick="event.preventDefault();
+                                                      document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <br><br>
+    @yield('content')
+</div>
+
+
+
+<br><br><br>
+<footer>
+    <! --- FOOTER & social links --->
+
+    <h5>2016 &copy by:   <a href="https://bitbucket.org/mmaniek95/">  Marta Mańka  <i class="fa fa-bitbucket fa-fw fa-1x"></i></a>
+        <a href="https://bitbucket.org/kamilJur/">  Kamil Jureczka  <i class="fa fa-bitbucket fa-fw fa-1x"></i></a>
+        <a href="https://bitbucket.org/lcieply/">  Łukasz Ciepły  <i class="fa fa-bitbucket fa-fw fa-1x"></i></a>
+        <a href="https://bitbucket.org/radopopulos/">  Radosław Ciupek  <i class="fa fa-bitbucket fa-fw fa-1x"></i></a>
+    </h5>
+
+</footer>
 </body>
 </html>
--- trunk/resources/views/welcome.blade.php
+++ trunk/resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.app')
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,23 +14,12 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
 
-
 </head>
 <! --- MENU  --->
 <body>
-
+@section('content')
 <div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div id="menu" class="top-right links">
-            @if (Auth::check())
-                <a href="{{ url('/home') }}">STRONA GŁÓWNA</a>
-            @else
-                <a href="{{ url('/login') }}">ZALOGUJ</a>
-                <a href="{{ url('/register') }}">REJESTRACJA</a>
-            @endif
-        </div>
 
-    @endif
         <! --- PANEL --->
 
     <div id="panel" class="content">
@@ -41,28 +31,6 @@
         </div>
     </div>
 </div>
-<footer>
-    <! --- FOOTER & social links --->
-
-    <h5>2016 &copy by: Marta Mańka  Kamil Jureczka Łukasz Ciepły Radosław Ciupek </h5>
-
-<!---------------
-    <br>
-    <ul class="list-inline">
-        <li>
-            <a href="https://bitbucket.org/mmaniek95/"><i class="fa fa-bitbucket fa-fw fa-3x"></i></a>
-        </li>
-        <li>
-            <a href="https://bitbucket.org/kamilJur/"> <i class="fa fa-bitbucket fa-fw fa-3x"></i></a>
-        </li>
-        <li>
-            <a href="https://bitbucket.org/lcieply/"><i class="fa fa-bitbucket fa-fw fa-3x"></i></a>
-        </li>
-        <li>
-            <a href="https://bitbucket.org/radopopulos/"> <i class="fa fa-bitbucket fa-fw fa-3x"></i></a>
-        </li>
-    </ul>
-------------!>
-</footer>
+@endsection
 </body>
 </html>
