9835c0e483c86037823b4588e32005acd89d0d51
student <marta.manka.95@gmail.com>
2016-12-15 11:23:35 +0100

    TASK 85 - BUG - Kalendarz wchodzi na stopkę (widoczne na localhost:8000/users/1)  - troszke musze poprawic tlo :) popr1

--- trunk/resources/views/users/show.blade.php
+++ trunk/resources/views/users/show.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('script')
-    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}"/>
     <script src="{{ URL::asset('js/calendar/jquery.min.js') }}"></script>
     <script src="{{ URL::asset('js/calendar/moment.min.js') }}"></script>
     <script src="{{ URL::asset('js/calendar/calendar.min.js') }}"></script>
     <link rel="stylesheet" href="{{ URL::asset('css/calendar.min.css') }}"/>
     <script>
         $('#calendar').fullCalendar({
-            windowResize: function(view) {
+            windowResize: function (view) {
                 alert('The calendar has adjusted to a window resize');
             }
         });
@@ -26,46 +26,46 @@
                     <div class="panel-body">
 
 
-                           <label for="email" class="col-md-4 control-label">   Adres e-mail: </label>
-                                <div class="col-md-6">
-                            <input name="title" class="form-control" VALUE=" {{$user->email}}" size="10"readonly><br>
+                        <label for="email" class="col-md-4 control-label"> Adres e-mail: </label>
+                        <div class="col-md-6">
+                            <input name="title" class="form-control" VALUE=" {{$user->email}}" size="10" readonly><br>
 
-</div>
+                        </div>
 
-                                    <label for="city" class="col-md-4 control-label">   Miasto: </label>
-                                    <div class="col-md-6">
+                        <label for="city" class="col-md-4 control-label"> Miasto: </label>
+                        <div class="col-md-6">
 
-                                    <input name="city" class="form-control" VALUE=" {{$user->city}}" size="10"readonly><br>
+                            <input name="city" class="form-control" VALUE=" {{$user->city}}" size="10" readonly><br>
 
 
                         </div>
 
 
-                       <label for="address" class="col-md-4 control-label">   Adres: </label>
-                                <div class="col-md-6">
-                                    <input name="address" class="form-control" VALUE=" {{$user->address}}" size="10"readonly><br>
-
-                         
-
-                        </div>
-
-                                    <label for="email" class="col-md-4 control-label">   Telefon: </label>
-                                <div class="col-md-6">
-                                    <input name="title" class="form-control" VALUE=" {{$user->phone}}" size="10"readonly><br>
+                        <label for="address" class="col-md-4 control-label"> Adres: </label>
+                        <div class="col-md-6">
+                            <input name="address" class="form-control" VALUE=" {{$user->address}}" size="10"
+                                   readonly><br>
 
 
                         </div>
 
+                        <label for="email" class="col-md-4 control-label"> Telefon: </label>
+                        <div class="col-md-6">
+                            <input name="title" class="form-control" VALUE=" {{$user->phone}}" size="10" readonly><br>
 
-                            <div id="kalendarz"  class="col-md-9 ">
-                                    <br>
-                                    {!! $calendar->calendar() !!}
-                                    {!! $calendar->script() !!}
-                                    <br>
-                                    <div class="col-md-8 col-md-offset-2">
-                                        <a href="{{url()->previous()}}" value="home" class="btn btn-primary pull-left">Back</a>
-                                        <a href="{{route('meetings.create')}}" class="btn btn-primary  ">New meeting</a>
-                                        <br><br>
+
+                        </div>
+
+
+                        <div id="kalendarz" class="col-md-9 ">
+                            <br>
+                            {!! $calendar->calendar() !!}
+                            {!! $calendar->script() !!}
+                            <br>
+                            <div class="col-md-8 col-md-offset-2">
+                                <a href="{{url()->previous()}}" value="home" class="btn btn-primary pull-left">Back</a>
+                                <a href="{{route('meetings.create')}}" class="btn btn-primary  ">New meeting</a>
+                                <br><br>
 
                             </div>
                         </div>
@@ -74,7 +74,7 @@
             </div>
         </div>
     </div>
-   </div>
-        </div>
+    </div>
+    </div>
     <br><br>
 @endsection
