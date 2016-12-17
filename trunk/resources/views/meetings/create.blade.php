f734739dfeabd2d7a68377bbeddddf35ce54a4f7
Łukasz Ciepły <lukaszcieply94@gmail.com>
2016-12-17 14:10:11 +0100

    TASK 61 - Zmiana wygladu w meetings/create

--- trunk/resources/views/meetings/create.blade.php
+++ trunk/resources/views/meetings/create.blade.php
@@ -30,31 +30,56 @@
                 </div>
                 <div class="panel panel-default">
                     <div class="panel-heading">New Meeting</div>
-                    <div>
-                        <form action="{{route('meetings.store')}}" method="post">
-                            <label for="allday">All day</label>
-                            <input type="checkbox" id="allDayCheck" name="allday" value="allday" onclick="hideElements()" />
-                            <br>
-                            <label for="start_date">Start date</label>
-                            <input type="date" name="start_date" value="{{ old('start_date') }}">
-                            <br>
-                            <label for="start_time" class = "toHide">Start time</label>
-                            <input type="time" name="start_time" class = "toHide" value="{{ old('start_time') }}">
-                            <br>
-                            <label for="end_date" class = "toHide">End date</label>
-                            <input type="date" name="end_date" class = "toHide" value="{{ old('end_date') }}">
-                            <br>
-                            <label for="end_time" class = "toHide">End time</label>
-                            <input type="time" name="end_time" class = "toHide" value="{{ old('end_time') }}">
-                            <br>
-                            <label for="private">Private</label>
-                            <input type="checkbox" name="private" value="private" />
-                            <br>
+                    <div class="panel-body">
+                        <form action="{{route('meetings.store')}}" method="post" form-horizontal class="form-horizontal">
+                            <div class="form-group">
+                                <label for="name" class="col-md-4 control-label">All day</label>
+                                <div class="col-md-6">
+                                    <input type="checkbox" id="allDayCheck" name="allday" value="0" onclick="hideElements()"/>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="start_date" class="col-md-4 control-label">Start date</label>
+                                <div class="col-md-6">
+                                    <input type="date" name="start_date" class="form-control"
+                                           value="{{ old('start_date') }}">
+                                </div>
+                            </div>
+                            <div class="form-group toHide">
+                                <label for="start_time" class="col-md-4 control-label">Start time</label>
+                                <div class="col-md-6">
+                                    <input type="time" name="start_time" class="form-control"
+                                           value="{{ old('start_time') }}">
+                                </div>
+                            </div>
+                            <div class="form-group toHide">
+                                <label for="end_date" class="col-md-4 control-label">End date</label>
+                                <div class="col-md-6">
+                                    <input type="date" name="end_date" class="form-control"
+                                           value="{{ old('end_date') }}">
+                                </div>
+                            </div>
+                            <div class="form-group toHide">
+                                <label for="end_time" class="col-md-4 control-label">End time</label>
+                                <div class="col-md-6">
+                                    <input type="time" name="end_time" class="form-control"
+                                           value="{{ old('end_time') }}">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="name" class="col-md-4 control-label">Private</label>
+                                <div class="col-md-6">
+                                    <input type="hidden" value="0" name="private"/>
+                                    <input type="checkbox" value="1" name="private"/>
+                                </div>
+                            </div>
                             <input type="text" name="user2_id" value="{{$id}}" hidden>
                             <input type="text" name="start" value="" hidden>
                             <input type="text" name="end" value="" hidden>
+                            <div class="col-md-8 col-md-offset-2 pull-left">
                             <input type="submit" value="Create" class="btn btn-primary pull-right">
                             {{csrf_field()}}
+                            </div>
                         </form>
                     </div>
                     <div class="panel-body">
