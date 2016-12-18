0245ae0c22ffc67f22cc26e896564ab72f4fb6f5
student <marta.manka.95@gmail.com>
2016-12-18 11:20:17 +0100

    TASK 80 - Metoda search w UserControllerze powinna wyszukać użytkowników pasujących do wpisanych danych -jeszcze nie odporny na zmiane kolejnosci wpisywanych danych

--- trunk/resources/views/users/show.blade.php
+++ trunk/resources/views/users/show.blade.php
@@ -1,29 +1,21 @@
 @extends('layouts.app')
 
-@section('script')
-    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}"/>
-    <script src="{{ URL::asset('js/calendar/jquery.min.js') }}"></script>
-    <script src="{{ URL::asset('js/calendar/moment.min.js') }}"></script>
-    <script src="{{ URL::asset('js/calendar/calendar.min.js') }}"></script>
-    <link rel="stylesheet" href="{{ URL::asset('css/calendar.min.css') }}"/>
-    <script>
-        $('#calendar').fullCalendar({
-            windowResize: function (view) {
-                alert('The calendar has adjusted to a window resize');
-            }
-        });
-    </script>
-@endsection
+
 
 @section('content')
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}"/>
     <br><br><br><br>
+
+
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">{{$user->name}} {{$user->surname}}</div>
                     <div class="panel-body">
+
+
+
 
 
                         <label for="email" class="col-md-4 control-label"> Adres e-mail: </label>
@@ -63,18 +55,27 @@
                             {!! $calendar->script() !!}
                             <br>
                             <div class="col-md-8 col-md-offset-2">
-                                <a href="{{url()->previous()}}" value="home" class="btn btn-primary pull-left">Back</a>
+                                <a href="{{url()->previous()}}" value="search" class="btn btn-primary pull-left">Back</a>
                                 <a href="{{route('meetings.create', $user->id)}}" class="btn btn-primary  ">New meeting</a>
                                 <br><br>
 
                             </div>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
+
+
     </div>
     </div>
     </div>
+
+
+        <h1> This user hasn't exist yet!!!</h1>
     <br><br>
+
+
 @endsection
