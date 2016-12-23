5ab960438ecce2028e2a815c8e41aeec8afe39ea
student <marta.manka.95@gmail.com>
2016-12-23 20:20:24 +0100

    README

--- trunk/resources/views/auth/register.blade.php
+++ trunk/resources/views/auth/register.blade.php
@@ -120,7 +120,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" >
                                     Register
                                 </button>
                             </div>
