ac8414fe63fbb124bdd876b49a981f6959ec8f1d
student <marta.manka.95@gmail.com>
2016-12-23 18:02:19 +0100

    Poprawka wygladu meetings create.blade cz.2

--- trunk/resources/views/meetings/create.blade.php
+++ trunk/resources/views/meetings/create.blade.php
@@ -38,139 +38,136 @@
                     <div class="panel-body">
 
 
-                            <div class="col-md-5">
-                                <label class="control-label panel-heading"> You can specify a time manualy</label>
+                        <div class="col-md-5">
+                            <label class="control-label panel-heading"> You can specify a time manualy</label>
 
 
-                                <form action="{{route('meetings.store')}}" method="post">
+                            <form action="{{route('meetings.store')}}" method="post">
 
-                                        <label for="start_date" class="col-md-4 control-label">Start date</label>
-                                        <div class="col-md-8">
-                                            <input type="date" name="start_date" class="form-control"
-                                                   value="{{ old('start_date') }}">
-                                            <br>
-                                        </div>
+                                <label for="start_date" class="col-md-4 control-label">Start date</label>
+                                <div class="col-md-8">
+                                    <input type="date" name="start_date" class="form-control"
+                                           value="{{ old('start_date') }}">
+                                    <br>
+                                </div>
 
-                                        <label for="allday" class="col-md-4 control-label">All day</label>
+                                <label for="allday" class="col-md-4 control-label">All day</label>
 
-                                        <div class="col-md-8">
-                                            <input type="hidden"  value="0" name="allday"/>
+                                <div class="col-md-8">
+                                    <input type="hidden" value="0" name="allday"/>
 
-                                            <input type="checkbox"  id="allDayCheck" name="allday"
-                                                   value="1" onclick="hideElements()"/>
-                                            <br><br>
-                                        </div>
-
-
-
-
-
-                                        <label for="start_time" class="col-md-4 toHide control-label ">Start
-                                            time</label>
-                                        <div class="col-md-8 toHide">
-                                            <input type="time" name="start_time" class="form-control toHide"
-
-                                                   value="{{ old('start_time') }}">
-                                            <br>
-
-                                    </div>
-
-                                        <label for="end_date" class="col-md-4 toHide control-label">End date</label>
-                                        <div class="col-md-8 toHide">
-                                            <input type="date" name="end_date" class="toHide form-control"
-                                                   value="{{ old('end_date') }}">
-                                            <br>
-
-                                    </div>
-
-                                        <label for="end_time" class="col-md-4 toHide control-label">End time</label>
-                                        <div class="col-md-8 toHide" >
-                                            <input type="time" name="end_time" class=" form-control "
-                                                   value="{{ old('end_time') }}">
-                                            <br>
-
-                                    </div>
-
-                                        <label for="private" class="col-md-4 control-label">Private</label>
-                                        <div class="col-md-8">
-                                            <input type="hidden" value="0" name="private"/>
-                                            <input type="checkbox" name="private" value="private"/>
-                                            <br><br>
-
-                                    </div>
-                                    <input type="text" name="user2_id" value="{{$id}}" hidden>
-                                    <input type="text" name="start" value="" hidden>
-                                    <input type="text" name="end" value="" hidden>
-
-
-                                        <div class="col-md-12">
-
-                                            <input type="submit" value="Create" class="btn btn-primary"><br>
-                                            {{csrf_field()}}
-                                        </div>
-
-                                </form>
-                            </div>
-
-
-                            <div class="col-md-2">
-                                <br><br>
-                                <h4>or</h4>
-
-                                <div class="col-md-12">
-                                <a href="{{route('users.show', $id)}}" class="btn btn-primary">Back</a>
-</div>
+                                    <input type="checkbox" id="allDayCheck" name="allday"
+                                           value="1" onclick="hideElements()"/>
+                                    <br><br>
                                 </div>
 
 
-                                <div class="col-md-5">
+                                <label for="start_time" class="col-md-4 toHide control-label ">Start
+                                    time</label>
+                                <div class="col-md-8 toHide">
+                                    <input type="time" name="start_time" class="form-control toHide"
 
-                                        <label class="control-label panel-heading"> Let me do that for you</label>
-                                        <form action="{{route('meetings.search', $id)}}" method="post">
+                                           value="{{ old('start_time') }}">
+                                    <br>
 
-                                            <label for="date" class="col-md-4  control-label">Date</label>
-                                            <div class="col-md-8">
-                                                <input type="date" name="date" class="form-control"
-                                                       value="{{ old('date') }}">
-                                                <br>
-                                        </div>
-                                        <div>
-                                            <label for="time" class="col-md-4  control-label">Between</label>
-                                            <div class="col-md-8">
-                                                <input type="time" name="time" class="form-control"
-                                                       value="{{ old('time') }}">
-                                                <br>
-                                            </div>
-                                            <label for="time2" class="col-md-4 control-label">and</label>
-                                            <div class="col-md-8">
-                                                <input type="time" name="time2" class="form-control"
-                                                       value="{{ old('time2') }}">
-                                                <br>
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <label for="duration" class="col-md-4 control-label">Duration</label>
-                                            <div class="col-md-8">
-                                                <input type="time" name="duration" class="form-control"
-                                                       value="{{ old('duration') }}">
-                                                <br>
-                                            </div>
-                                        </div>
+                                </div>
 
-                                            <label for="private2" class="col-md-4 control-label">Private</label>
-                                            <div class="col-md-8">
-                                                <input type="hidden" value="0"  name="private2"/>
-                                                <input type="checkbox" name="private2"  value="private2"/>
-                                               <br><br>
-                                            </div>
+                                <label for="end_date" class="col-md-4 toHide control-label">End date</label>
+                                <div class="col-md-8 toHide">
+                                    <input type="date" name="end_date" class="toHide form-control"
+                                           value="{{ old('end_date') }}">
+                                    <br>
 
-                                        <div class="col-md-12">
+                                </div>
 
-                                            <input type="submit" value="Search" class="btn btn-primary ">
-                                            {{csrf_field()}}
-                                        </div>
+                                <label for="end_time" class="col-md-4 toHide control-label">End time</label>
+                                <div class="col-md-8 toHide">
+                                    <input type="time" name="end_time" class=" form-control "
+                                           value="{{ old('end_time') }}">
+                                    <br>
 
-                                    </form>
+                                </div>
+
+                                <label for="private" class="col-md-4 control-label">Private</label>
+                                <div class="col-md-8">
+                                    <input type="hidden" value="0" name="private"/>
+                                    <input type="checkbox" name="private" value="private"/>
+                                    <br><br>
+
+                                </div>
+                                <input type="text" name="user2_id" value="{{$id}}" hidden>
+                                <input type="text" name="start" value="" hidden>
+                                <input type="text" name="end" value="" hidden>
+
+
+                                <div class="col-md-12">
+
+                                    <input type="submit" value="Create" class="btn btn-primary"><br>
+                                    {{csrf_field()}}
+                                </div>
+
+                            </form>
+                        </div>
+
+
+                        <div class="col-md-2">
+                            <br><br>
+                            <h4>or</h4>
+
+                            <div class="col-md-12">
+                                <a href="{{route('users.show', $id)}}" class="btn btn-primary">Back</a>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-5">
+
+                            <label class="control-label panel-heading"> Let me do that for you</label>
+                            <form action="{{route('meetings.search', $id)}}" method="post">
+
+                                <label for="date" class="col-md-4  control-label">Date</label>
+                                <div class="col-md-8">
+                                    <input type="date" name="date" class="form-control"
+                                           value="{{ old('date') }}">
+                                    <br>
+                                </div>
+                                <div>
+                                    <label for="time" class="col-md-4  control-label">Between</label>
+                                    <div class="col-md-8">
+                                        <input type="time" name="time" class="form-control"
+                                               value="{{ old('time') }}">
+                                        <br>
+                                    </div>
+                                    <label for="time2" class="col-md-4 control-label">and</label>
+                                    <div class="col-md-8">
+                                        <input type="time" name="time2" class="form-control"
+                                               value="{{ old('time2') }}">
+                                        <br>
+                                    </div>
+                                </div>
+                                <div>
+                                    <label for="duration" class="col-md-4 control-label">Duration</label>
+                                    <div class="col-md-8">
+                                        <input type="time" name="duration" class="form-control"
+                                               value="{{ old('duration') }}">
+                                        <br>
+                                    </div>
+                                </div>
+
+                                <label for="private2" class="col-md-4 control-label">Private</label>
+                                <div class="col-md-8">
+                                    <input type="hidden" value="0" name="private2"/>
+                                    <input type="checkbox" name="private2" value="private2"/>
+                                    <br><br>
+                                </div>
+
+                                <div class="col-md-12">
+
+                                    <input type="submit" value="Search" class="btn btn-primary ">
+                                    {{csrf_field()}}
+                                </div>
+
+                            </form>
 
 
                         </div>
@@ -179,6 +176,6 @@
                 </div>
             </div>
         </div>
-</div>
+    </div>
     <br><br>
 @endsection
